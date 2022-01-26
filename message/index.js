a657b93f77769d910f5b0870411907bdbcca2577
mohamed <mohamed.mahmoud@camelcasetech.com>
2022-01-26 13:59:21 +0200

    create new functions to send message and  email

---
+++ message/index.js
@@ -0,0 +1,30 @@
+const AWS = require('aws-sdk');
+exports.handler = async (event) => {
+    const body = JSON.parse(event.body);
+    const sqs = new AWS.SQS();
+    const params = {
+            MessageAttributes: {
+                "Subject": {
+                    DataType: "String",
+                    StringValue: body.subject
+                }
+            },
+            MessageBody: body.text,
+            QueueUrl: "https://sqs.eu-central-1.amazonaws.com/310796145073/message"
+        };
+    try {
+            const data = await sqs.sendMessage(params).promise();
+            console.log("Success", data.MessageId);
+            const response = {
+                statusCode: 200,
+                body: JSON.stringify({MessageId: data.MessageId}),
+            };
+            return response;
+    } catch (error) {
+        const response = {
+            statusCode: 409,
+            body: JSON.stringify({error}),
+        };
+        return response;
+    }
+};
---
+++ sendMail/index.js
@@ -0,0 +1,40 @@
+const AWS = require('aws-sdk');
+exports.handler = async (event) => {
+    const sqs = new AWS.SQS();
+    const S3 = new AWS.S3();
+    let content = [];
+    const options = {
+         Bucket: 'newsletter-subscribe',
+         Key: 'users.json',
+    };
+    try {
+        const params = {
+            QueueUrl: 'https://sqs.eu-central-1.amazonaws.com/310796145073/message'
+        };
+        const response = {
+            statusCode: 200,
+            body: JSON.stringify('Success!')
+            };
+        const message = await sqs.receiveMessage(params).promise();
+        if (message) {
+            const data = await S3.getObject(options).promise();
+            if (data) {
+                content = JSON.parse(data.Body.toString('utf-8'));
+                for(const item of content) {
+                    //send eamil
+                }
+            }
+            response.body = JSON.stringify({message : message.Messages});
+        
+        } else {
+            response.body = JSON.stringify('There are no messages!');
+        }
+        return response;
+    } catch (error) {
+        const fail = {
+        statusCode: 409,
+        body: JSON.stringify({error}),
+    };
+    return fail
+    }
+};
