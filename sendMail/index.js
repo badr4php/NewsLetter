6287bdc7a2b9b5bb285af902637f15b2a15afab7
mohamed <mohamed.mahmoud@camelcasetech.com>
2022-01-27 13:30:16 +0200

    edit send email function

--- sendMail/index.js
+++ sendMail/index.js
@@ -1,40 +1,50 @@
 const AWS = require('aws-sdk');
 exports.handler = async (event) => {
-    const sqs = new AWS.SQS({region: 'us-east-1'});
     const S3 = new AWS.S3();
+    const ses = new AWS.SES();
     let content = [];
     const options = {
          Bucket: 'newsletter-subscribe',
          Key: 'users.json',
     };
+    const messageOptions = {
+            Bucket: 'newsletter-subscribe',
+            Key: 'message.csv',
+            ContentType: 'text/plain'
+    };
     try {
-        const params = {
-            QueueUrl: 'https://sqs.us-east-1.amazonaws.com/310796145073/message'
-        };
-        const response = {
-            statusCode: 200,
-            body: JSON.stringify('Success!')
-            };
-        const message = await sqs.receiveMessage(params).promise();
-        if (message) {
             const data = await S3.getObject(options).promise();
             if (data) {
                 content = JSON.parse(data.Body.toString('utf-8'));
+                const bodyContent = [];
+                const emails = [];
                 for(const item of content) {
-                    //send eamil
+                    emails.push(item.email);
                 }
+                event.Records.forEach(record => {
+                    bodyContent.push(record.body);
+                    const sesParams = {
+                        Destination: {
+                            ToAddresses: emails,
+                        },
+                        Message: {
+                            Body: {
+                                Text: { Data: record.body },
+                            },
+
+                            Subject: { Data: record.messageAttributes.Subject.stringValue },
+                        },
+                        Source: "mohamed.mahmoud@camelcasetech.com",
+                    };
+ 
+                    const sesResponse =  ses.sendEmail(sesParams).promise()
+                });
+                const s3Response = await S3.upload({...messageOptions, Body: JSON.stringify({bodyContent, emails})}).promise();
+                
+                console.log(JSON.stringify({message : bodyContent}));
             }
-            response.body = JSON.stringify({message : message.Messages});
         
-        } else {
-            response.body = JSON.stringify('There are no messages!');
-        }
-        return response;
     } catch (error) {
-        const fail = {
-        statusCode: 409,
-        body: JSON.stringify({error}),
-    };
-    return fail
+        console.log(error);
     }
 };
